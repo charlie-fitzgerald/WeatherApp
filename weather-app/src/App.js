5fcfe1913697de4f949e4ef6136b0a44a481b95e
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2024-08-30 19:41:51 -0700

    Add styles

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,8 @@
 import Content from "./components/content"
-import './App.css';
+const api = {
+  key: "4213f55cb14fe7e4acd06dbf54f8ea21",
+  base: `https://api.openweathermap.org/data/2.5/`
+}
 
 function App() {
   return (
--- weather-app/src/components/content.js
+++ weather-app/src/components/content.js
@@ -3,24 +3,42 @@ import React from 'react'
 export default function Content() {
     const [state, setState] = React.useState("")
 
-    const handleChange = event => {
-        setState(event.target.value)
+    const dateBuilder = d => {
+        let months=["January",'February','March','April','May','June','July','August','September','October','November','December'];
+        let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${month} ${date}, ${year}`
     }
 
+
     return ( 
-        <div>
-            <h1>Enter Text</h1>
-            <br />
-            <form>
-                <input 
+        <div className="app">
+            <main>
+                <div className="search-box">
+                    <input 
                     type="text"
-                    placeholder="Enter Text"
-                    onChange={handleChange}
-                />
-                <input type="submit"/>
-            </form>
-            <br />
-            <h1>{state}</h1>
+                    className="search-bar"
+                    placeholder="Search..."
+                    />
+
+                </div> 
+
+                <div className="location-box">
+                    <div className="location">New York City, USA</div>
+                    <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+
+                <div className="weather-box">
+                    <div className="temp">15°C</div>
+                    <div className="weather">Sunny</div>
+                </div>
+                
+            </main>
         </div>      
         
         
